0bee95266649128355a2fb93fcea1d70e65bff3f
james641 <james641@163.com>
2015-10-01 14:23:38 +0800

    create simplefactory程序集

---
+++ SimpleFactory/CreateFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    public class CreateFactory
+    {
+        public static ICar GetObject(string objName)
+        {
+            switch (objName)
+            {
+                case "Benz":
+                    return new Benz();
+                case "Bmw":
+                    return new Bmw();
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+    }
+}
---
+++ TestFactory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleFactory;
+
+namespace TestFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ICar myCar = CreateFactory.GetObject("Bmw");
+            myCar.Drive();
+
+
+            Console.ReadKey();
+        }
+    }
+}
